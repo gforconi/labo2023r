209e9e009d97e18246a648f48bc178b7e9bcf6fd
gforconi <geronimoforconi@gmail.com>
2023-07-03 15:04:37 -0300

    Segunda corrida

--- src/workflow-inicial/631_FE_historia.r
+++ src/workflow-inicial/631_FE_historia.r
@@ -55,7 +55,7 @@ PARAM$RandomForest$semilla <- 116257 # cambiar por la propia semilla
 # varia de 0.0 a 2.0, si es 0.0 NO se activan
 PARAM$CanaritosAsesinos$ratio <- 0.66
 # desvios estandar de la media, para el cutoff
-PARAM$CanaritosAsesinos$desvios <- 4.0
+PARAM$CanaritosAsesinos$desvios <- 3.0
 # cambiar por la propia semilla
 PARAM$CanaritosAsesinos$semilla <- 116257
 
